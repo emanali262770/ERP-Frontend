710ff567a0e07c751fe92410327d053ce61d74fd
Ali Gates <hacktech877@gmail.com>
2025-09-30 13:10:47 +0500

    aa

--- src/pages/admin/AdminSidebar.jsx
+++ src/pages/admin/AdminSidebar.jsx
@@ -73,11 +73,6 @@ const links = [
         label: "Purchase Order",
         icon: <FaFileInvoiceDollar />,
       },
-       {
-        to: "/admin/purchase-order",
-        label: "I love Order",
-        icon: <FaFileInvoiceDollar />,
-      },
       { to: "/admin/gatepass-in", label: "GatePass IN", icon: <FaSignInAlt /> },
       {
         to: "/admin/quality-checking",
