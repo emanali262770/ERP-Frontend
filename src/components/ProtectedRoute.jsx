346d1e3c59354c2449524d3fbda49e4c3c4c8ad9
zainsultani333-glitch <zainsultani333@gmail.com>
2025-10-06 16:13:41 +0500

    new

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,12 +1,43 @@
 // src/components/ProtectedRoute.jsx
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, role }) => {
+  const [isValid, setisValid] = useState(null);
   const user = JSON.parse(localStorage.getItem("userInfo"));
-  
+  const API_URL = "https://erp-backend-drab.vercel.app/api/token/check";
 
-  if (!user) return <Navigate to="/" />;
+  useEffect(() => {
+    const vercelCheck = async () => {
+      if (!user || !user.token) {
+        setisValid(false);
+        return;
+      }
+
+      try {
+        const response = await axios.get(API_URL, {
+          headers: { Authorization: `Bearer ${user.token}` },
+        });
+        console.log({ response });
+
+        if (response.data.success === false) {
+          localStorage.clear();
+          setisValid(false);
+        } else {
+          setisValid(true);
+        }
+      } catch (error) {
+        console.error("Token validation failed:", error);
+        localStorage.clear();
+        setisValid(false);
+      }
+    };
+    vercelCheck();
+  }, []);
+  if (isValid === null) return null;
+  if (!user || !isValid) return <Navigate to="/" replace />;
   if (role && user.role !== role) return <Navigate to="/" />;
 
   return children;
